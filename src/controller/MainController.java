eaaf7694431060007edbf73ef525fcd2606f5a19
JunyiZHONG <zhongjunyi.dev@gmail.com>
2019-05-31 15:39:07 +0200

    test git

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	static{
-		System.out.println("hello????");
+		System.out.println("Bonjour!");
 	}
     @RequestMapping(value="/")
     public String hello1(){
@@ -17,7 +17,7 @@ public class MainController {
     @RequestMapping(value="/home")
     public String hello(Model model){
     	System.out.println("/world called.");
-        model.addAttribute("msg", "你好spring mvc");
+        model.addAttribute("msg", "hi spring mvc");
         return "home";
     }
 }
